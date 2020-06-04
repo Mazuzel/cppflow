8e04f58c8a49f01dc5d70101d8944931fd9a1994
mazuzel <mazuzel@protonmail.com>
2020-06-04 17:24:00 +0200

    Added pix2pix example

--- examples/pix2pix/main.cpp
+++ examples/pix2pix/main.cpp
@@ -29,38 +29,33 @@ int main() {
 
     // Read image
     img = cv::imread("../images/test_image.png", cv::IMREAD_COLOR);
-	
+
     cv::resize(img, inp, cv::Size(imgCols, imgRows));
     cv::cvtColor(img, inp, cv::COLOR_BGR2RGB);
 	
     std::vector<float> img_data;
 
-    for(int y=0;y<img.rows;y++)
-      {
-	for(int x=0;x<img.cols;x++)
-	  {
-	    Vec3b & color = inp.at<Vec3b>(y,x);
-	    for(int channel=0; channel<3; channel++)
-	      {
-		// Scale image to range [0, 1]
-		img_data.push_back((float)(color[channel]/255.0));
-	      }
-	  }
+    // Scale image to range [0, 1]
+    for(int y=0;y<img.rows;y++){
+      for(int x=0;x<img.cols;x++){
+	Vec3b & color = inp.at<Vec3b>(y,x); //not optimal, to be improved
+	for(int channel=0; channel<3; channel++){
+	  img_data.push_back((float)(color[channel]/255.0));
+	}
       }
-	
-    //img_data.assign(scaled.data, scaled.data + scaled.total() * scaled.channels());
+    }
+
     input.set_data(img_data, {1, imgCols, imgRows, 3});
 
     // Run and show predictions
     m.run(input, prediction);
 
-    // Get tensor with predictions
+    // Get tensor with output image
     std::vector<float> imgVector = prediction.Tensor::get_data<float>();
-	
+
     unsigned char* pixels = (unsigned char*)malloc(sizeof(unsigned char) * imgRows*imgCols*3);
     for (int j = 0; j < imgVector.size(); j++){
-      float val = imgVector[j];
-      pixels[j] = (unsigned char)((val+1.0)*127.5);
+      pixels[j] = (unsigned char)((imgVector[j]+1.0)*127.5);
     }
     cv::Mat image(imgRows, imgCols, CV_8UC3, pixels);
     cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
