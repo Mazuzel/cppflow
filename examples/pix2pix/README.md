296c6618c625fdf6b8f71e8309b61e8663b609d6
mazuzel <mazuzel@protonmail.com>
2020-06-04 17:09:44 +0200

    Added pix2pix example

---
+++ examples/pix2pix/README.md
@@ -0,0 +1,1 @@
+See [https://github.com/memo/webcam-pix2pix-tensorflow] for instructions on how to train a pix2pix model.
---
+++ examples/pix2pix/main.cpp
@@ -0,0 +1,69 @@
+//
+// Created by mazuzel on 04/06/2020.
+//
+
+#include "../../include/Model.h"
+#include "../../include/Tensor.h"
+#include <opencv2/opencv.hpp>
+#include <algorithm>
+#include <iterator>
+
+#include <stdio.h>
+
+using namespace cv;
+
+int main() {
+
+    // Create model
+    Model m("../graph_frz.pb");
+
+    // Create Tensors
+    Tensor input(m, "generator/generator_inputs");
+    Tensor prediction(m, "generator/generator_outputs");
+
+    // Read image
+    int imgRows = 256;
+    int imgCols = 256;
+
+    cv::Mat img, inp, scaled;
+
+    // Read image
+    img = cv::imread("../images/test_image.png", cv::IMREAD_COLOR);
+	
+    cv::resize(img, inp, cv::Size(imgCols, imgRows));
+    cv::cvtColor(img, inp, cv::COLOR_BGR2RGB);
+	
+    std::vector<float> img_data;
+
+    for(int y=0;y<img.rows;y++)
+      {
+	for(int x=0;x<img.cols;x++)
+	  {
+	    Vec3b & color = inp.at<Vec3b>(y,x);
+	    for(int channel=0; channel<3; channel++)
+	      {
+		// Scale image to range [0, 1]
+		img_data.push_back((float)(color[channel]/255.0));
+	      }
+	  }
+      }
+	
+    //img_data.assign(scaled.data, scaled.data + scaled.total() * scaled.channels());
+    input.set_data(img_data, {1, imgCols, imgRows, 3});
+
+    // Run and show predictions
+    m.run(input, prediction);
+
+    // Get tensor with predictions
+    std::vector<float> imgVector = prediction.Tensor::get_data<float>();
+	
+    unsigned char* pixels = (unsigned char*)malloc(sizeof(unsigned char) * imgRows*imgCols*3);
+    for (int j = 0; j < imgVector.size(); j++){
+      float val = imgVector[j];
+      pixels[j] = (unsigned char)((val+1.0)*127.5);
+    }
+    cv::Mat image(imgRows, imgCols, CV_8UC3, pixels);
+    cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+	
+    imwrite("output.jpg", image);
+}
